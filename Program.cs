12f84f8ae94a22c465d9a35900e40fe9c85dfec3
Zaki Muhammad Wahyudi <zakimuhammad2427@students.amikom.ac.id>
2021-03-17 23:56:28 +0700

    Tugas Pemograman Membuat Taxi

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek Taxi
+            Taxi taxi = new Taxi();
+
+            //pengesetan nilai properties
+            taxi.DriverName = " Zaki Muhammad W ";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 99;
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassager();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
